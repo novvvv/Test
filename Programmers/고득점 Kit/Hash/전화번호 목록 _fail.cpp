3acd65684a3e2913ab3c45cc0df116a90d159270
nov <85823762+novvvv@users.noreply.github.com>
2022-09-16 17:14:46 +0900

    Update 전화번호 목록 _fail.cpp

--- Programmers/고득점 Kit/Hash/전화번호 목록 _fail.cpp
+++ Programmers/고득점 Kit/Hash/전화번호 목록 _fail.cpp
@@ -3,16 +3,14 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
     for(int i = 0; i < phone_book.size(); ++i){
         for(int j = 0; j < phone_book.size(); ++j){
-            if(phone_book[i] == phone_book[j]) continue;
             // i번째 문자열이 j번째 문자열의 접두어인 경우.
-            if(phone_book[i].find(phone_book[j]) == 0){
-                answer = false;
-                break;
-            } 
+            if(phone_book[i] != phone_book[j] && 
+               phone_book[i].find(phone_book[j]) == 0){
+                return false;
+            }
         }
     }
-    return answer;
+    return true;
 }
