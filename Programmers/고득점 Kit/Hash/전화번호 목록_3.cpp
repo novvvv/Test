e6955dbb20ba536cb2b4bf0dc22ec57ad7ccb29a
nov <85823762+novvvv@users.noreply.github.com>
2022-09-18 19:02:09 +0900

    Update 전화번호 목록_3.cpp

--- Programmers/고득점 Kit/Hash/전화번호 목록_3.cpp
+++ Programmers/고득점 Kit/Hash/전화번호 목록_3.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    unordered_set<string> phone_set;
-    for(int i = 0; i < phone_book.size(); ++i)
-        phone_set.insert(phone_book[i]);
-    
-    unordered_set<string>::iterator it;
+    unordered_set<string> phone_list;
+    for(const auto& i : phone_book)
+        phone_list.insert(i);
+
     for(int i = 0; i < phone_book.size(); ++i){
-        string phone_number = "";
+        string temp_str = "";
         for(int j = 0; j < phone_book[i].size(); ++j){
-            phone_number += phone_book[i][j];
-            it = phone_set.find(phone_number);
-            if(it != phone_set.end() && phone_number != phone_book[i]){
+            temp_str += phone_book[i][j];
+            if(phone_list.find(temp_str) != phone_list.end() &&
+              temp_str != phone_book[i]){
                 return false;
+                break;
             }
         }
     }
