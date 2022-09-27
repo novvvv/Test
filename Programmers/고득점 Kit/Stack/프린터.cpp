051a3ca71f2f816a96b8bd6401b38feccc5230ed
nov <85823762+novvvv@users.noreply.github.com>
2022-09-27 22:37:13 +0900

    Update 프린터.cpp

--- Programmers/고득점 Kit/Stack/프린터.cpp
+++ Programmers/고득점 Kit/Stack/프린터.cpp
@@ -11,12 +11,14 @@ int solution(vector<int> priorities, int location) {
     for(int i = 0; i < priorities.size(); ++i)
         printer.push(make_pair(priorities[i], i));
     while(!printer.empty()){
-        // priorities가 가장 높은 케이스
+        // 본인 순서 O
         if(printer.front().first == max_value){
+            // 본인 순서 O, 인덱스 O
             if(printer.front().second == location){
                 answer++;
                 break;
             }
+            // 본인 순서 O,  인덱스 X
             else{
                 priorities[printer.front().second] = 0;
                 max_value = *max_element(priorities.begin(), priorities.end());
@@ -25,7 +27,7 @@ int solution(vector<int> priorities, int location) {
             }
         }
         
-        // 자기 차례가 아닌 경우
+        // 본인 순서 X
         else{
             printer.push(printer.front());
             printer.pop();
