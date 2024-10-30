e386d3f2e3f4aedad5160e1fd99e5806a36468fa
nov <85823762+novvvv@users.noreply.github.com>
2024-10-30 21:32:56 +0900

    Create 2563.java

---
+++ BOJ/Java/2024/2563.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] paper = new int[100][100];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = 1;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j] == 1) sum++;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
