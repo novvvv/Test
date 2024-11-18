43e28094c4522d10ed2428924bb4da3ea7160d98
nov <85823762+novvvv@users.noreply.github.com>
2024-11-18 17:56:41 +0900

    Delete LeetCode directory

--- LeetCode/1929 Concatenation of Array.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>::iterator it = nums.insert(nums.end(), nums.begin(), nums.end());
-        return nums;
-    }
-};
