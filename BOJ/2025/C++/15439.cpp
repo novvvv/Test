0c0007b05da6395feb04e231f56e93840dd45631
nov <85823762+novvvv@users.noreply.github.com>
2025-01-11 08:16:15 +0900

    Create 15439.cpp

---
+++ BOJ/2025/C++/15439.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    cout << n * (n - 1) << '\n';
+
+    return 0;
+}
+
+
