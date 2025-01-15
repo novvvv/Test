f60a24349e867f9dd485e94d47f0ad28fd248df0
nov <85823762+novvvv@users.noreply.github.com>
2025-01-15 17:28:22 +0900

    Create STEP: 4 10 行の出力.cpp

---
+++ paiza/STEP: 4 10 行の出力.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    cout << 813 << endl;
+    cout << 1 << endl;
+    cout << 2 << endl;
+    cout << 923874 << endl;
+    cout << 23648 << endl;
+    cout << 782356 << endl;
+    cout << 3256 << endl;
+    cout << 2342 << endl;
+    cout << 24324 << endl;
+    cout << 112 << endl;
+
+    return 0;
+}
