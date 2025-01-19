7ea66ab8256e656056fe786d02f86e2764e35f05
doil <85823762+novvvv@users.noreply.github.com>
2025-01-20 05:31:09 +0900

    Create 24263.cpp

---
+++ BOJ/2025/C++/24263.cpp
@@ -0,0 +1,20 @@
+/**
+ * BOJ24263
+ * 2025.01.20
+ * nov2pro
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    cout << n << '\n' << 1 << '\n';
+
+    return 0;
+}
