3c82e1602d48178bb0ec9437b7c00251e5ddbae5
doil <85823762+novvvv@users.noreply.github.com>
2025-01-21 06:39:11 +0900

    Create 24264.cpp

---
+++ BOJ/2025/C++/24264.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+
+    cout << n * n << '\n' << 2 << '\n';
+
+    return 0;
+}
+
